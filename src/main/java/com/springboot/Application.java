fc6d76ffcc146938c780e90520ebd0c8358cee36
zhaishichong <zhaishichong@victorysoft.com.cn>
2024-05-11 11:26:42 +0800

    提交

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.springboot;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,5 +14,13 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        JSONObject param = new JSONObject();
+        param.put("aah","123456789101123456");
+        final String creditCode = param.getString("creditcode");
+        if (creditCode != null && !creditCode.isEmpty()) {
+            System.out.println(creditCode);
+        }else {
+            System.out.println("creditcode is null");
+        }
     }
 }
